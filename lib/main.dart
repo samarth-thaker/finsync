3b7fc6d5209d8a61c67efa2005be5a95918392a9
samarth-thaker <samarth.thaker07@gmail.com>
2025-06-27 16:20:39 +0530

    Github privacy issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,22 @@ import 'package:finsync_2/dashboard/dashboard.dart';
 import 'package:finsync_2/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,  
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  String? firebase_android = dotenv.env['FIREBASE_ANDROID'];
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,13 +28,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:Loginscreen(),
+      home: Loginscreen(),
       routes: {
-        '/login':(context)=>const Loginscreen(),
-        '/signup':(context)=>const SignupScreen(),
-        '/dashboard':(context)=>const DashboardScreen(),
+        '/login': (context) => const Loginscreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/dashboard': (context) => const DashboardScreen(),
       },
     );
   }
 }
-
